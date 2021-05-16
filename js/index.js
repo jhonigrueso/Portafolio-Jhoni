17e4ff29e3a60aa54712396cfd462ede82e64126
jhonigrueso <jhonigrueso2@gmail.com>
2021-05-16 12:37:41 -0500

    update

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const d = document;
+searchfilters(".card-filter", ".conter");
+
+function searchfilters(input, selector) {
+  d.addEventListener("keyup", (e) => {
+    if (e.target.matches(input) ) {
+      //console.log(e.key);
+      //console.log(e.target.value);
+      d.querySelectorAll(selector).forEach((el) =>
+        el.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+          ? el.classList.remove("filter")
+          : el.classList.add("filter")
+      );
+    } 
+  });
+}
