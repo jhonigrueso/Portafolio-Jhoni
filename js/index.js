a9409b18ecef069d863ae5875c9f946394813995
YonniGrueso123 <jhonigrueso2@gmail.com>
2022-04-09 19:45:30 -0500

    Updated Portafolio

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ function searchfilters(input, selector) {
       //console.log(e.key);
       //console.log(e.target.value);
       d.querySelectorAll(selector).forEach((el) =>
-        el.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-          ? el.classList.remove("filter")
-          : el.classList.add("filter")
+      el.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+      ? el.classList.remove("filter")
+      : el.classList.add("filter")
       );
-    } 
+    }
   });
 }
